a8ebf166e753bb96f8247a5e50ea2bc4a79f734d
patemit <patelmit8292@gmail.com>
2025-06-06 17:21:05 -0400

    Updated build number

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
-export const presets = ["babel-preset-expo"];
-export const plugins = [["module:react-native-dotenv"]];
-  
+module.exports = {
+  presets: ["babel-preset-expo"],
+  plugins: [["module:react-native-dotenv"]],
+};
